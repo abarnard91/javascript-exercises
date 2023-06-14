51f5e504dc55795ed2488c7c66d34ee4c8b3e815
Andrew Barnard <andrewbarnard09@gmail.com>
2023-06-13 20:55:30 -0500

    completed exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString="";
+    let num=string.length;
+    for (i=0;i<=string.length;i++){
+        newString+=string.charAt(num);
+        num-=1;
+    }
+    return newString;
 };
 
 // Do not edit below this line
