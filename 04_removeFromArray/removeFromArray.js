cd12c95a8be2195a46325f2c6e0f443c763b7253
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-06-15 11:07:52 -0500

    Update removeFromArray.js
    from stack overflow this works with all tests. my way fails when removing the first array element first.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,24 +1,3 @@
-const removeFromArray = function(theArray,...args) {
-  //iterates through arguments
-  for (let i=0;i<=args.length;i++){
-  	console.log(`i is ${i} args is ${args[i]}`);
-  	//for each argument iterate through the array to see if there's a match
-		for(let j=0;j<theArray.length;j++){
-    	console.log(`j is ${j} theArray is ${theArray[j]}`);
-      //if theres a match remove argument value from the array
-    	if(theArray[j]==args[i]){
-      	console.log(`the array is spliced ${theArray[j]}`)
-    		theArray.splice((theArray[j]-1),1)
-                
-        console.log(`spliced array is ${theArray}`)
-        }
-    	}
-		}
-    return(theArray);
-    console.log(theArray);
-	}
-
-};
-
+const removeFromArray = (arr, ...args) => arr.filter((item) => !args.includes(item))
 // Do not edit below this line
 module.exports = removeFromArray;
