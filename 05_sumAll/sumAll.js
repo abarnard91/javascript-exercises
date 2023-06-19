c58b7c807ed7a1c4b1605ef51fa382caf3de6659
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-06-19 14:58:31 -0500

    Update sumAll.js
    created function for adding low number until it hits high number

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum,secondNum) {
+  if (((firstNum<=0)||(secondNum<=0))||((Number.isInteger(firstNum)==false)||(Number.isInteger(secondNum)==false))){
+    console.log("ERROR")
+    return "ERROR";
+    }
+ 
+  	else if (firstNum>secondNum){
+    counterFunction(firstNum,secondNum);
+    }
+    else if (secondNum>firstNum){
+    counterFunction(secondNum,firstNum);
+    }
+    
+    	
+	}
 
-};
-
+function counterFunction(higherNum,lowerNum){
+let thirdNum=0;
+for (let i=0;i<higherNum;i++){
+      	thirdNum=lowerNum+thirdNum;
+        lowerNum++;
+  		}
+      console.log(thirdNum);
+      return thirdNum;
+}
 // Do not edit below this line
 module.exports = sumAll;
