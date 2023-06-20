23aa98463a5e09a663d9bcf73837aaa89fc8a59d
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-06-20 11:57:41 -0500

    Update tempConversion.js
    created 2 functions to convert celsius to fahrenheit and vice versa. 
    added corrections to round to 1 decimal if needed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,30 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+	let tempC=0;
+  //(32°F − 32) × 5/9 = 0°C
+  tempC=((fahrenheit-32)*(5/9));
+  //checks if the value of tempC is a whole number or decimal
+  if ((tempC%1)!=0){
+  //if decimal tempC is rounded to 1 decimal point
+  	tempC=tempC.toFixed(1);
+  }
+  console.log(`${fahrenheit} degrees fahrenheit is ${tempC} degrees celsius`);
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function(celsius) {
+	let tempF=0;
+  //(0°C × 9/5) + 32 = 32°F
+  tempF=(celsius*(9/5)+32)
+  //checks if the value of tempF is a whole number or decimal
+ 	if((tempF%1)!=0){
+  //if decimal tempF is rounded to 1 decimal point
+    tempF=tempF.toFixed(1);
+  }
+  console.log(`${celsius} degrees celsius is ${tempF} degrees fahrenheit`);
+  return tempF;
+  
 };
 
 // Do not edit below this line
