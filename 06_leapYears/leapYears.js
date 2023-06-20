4ad9e44d8e5e8f4f82c23a018370043672dbae04
abarnard91 <65411062+abarnard91@users.noreply.github.com>
2023-06-20 10:38:12 -0500

    Update leapYears.js
    created function to check if there is a remainder when a number is divided by 4, 100 and 400. 
    If no remainder for 4 but a remainder at 100 (for non century years) or no remainder for 400, then it is a leap year
    otherwise not a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  //if year/4 has no remainder and year/100 has a remainder or year/400 has no remainder
+	if(((year%4==0)&&(year%100!=0))||(year%400==0)){
+    //then return true it is a leap year
+  	console.log(`${year} is a leap year`)
+		return true;
+	}
+  else{
+    //otherwise it is not a leap year and false is returned
+  	console.log(`${year} is not a leap year`)
+  	return false;
+  }
 
 };
 
